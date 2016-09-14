4e4cdbf599c43fdda807fee4f8c9be7c49e627f0
= <=>
2016-09-14 00:40:12 -0500

    create default tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,9 @@
 /*jshint esversion: 6*/
-import { task } from 'gulp';
+
+// Import Gulp functions
+import { task, series, parallel, watch} from 'gulp';
+
+// Import Gulp tasks
 import { clean } from './gulp_modules/del';
 import { images } from './gulp_modules/imagemin';
 import { html } from './gulp_modules/metalsmith';
@@ -8,10 +12,11 @@ import { server } from './gulp_modules/browser-sync';
 import { styles } from './gulp_modules/stylus';
 import { vectors } from './gulp_modules/svg-symbols';
 
+// Clean out build director
 task(clean);
-task(images);
-task(html);
-task(scripts);
-task(server);
-task(styles);
-task(vectors);
+
+// Run default build task from command-line
+task('build', series(clean, html, parallel(scripts, styles, vectors)));
+
+// Start Gulp server from command-line
+task('serve', series('build', server));
